1800e40b9acddb49ed1af6bdd2dc2ab9cc76d817
andriii-droid <andri.andermatt@icloud.com>
2026-04-14 10:09:45 +0200

    library update

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 #include <Button.h>
 
 int relayPin = 36;
-int greenLEDPin = 2;
-int redLEDPin = 1;
+int greenLEDPin = 1;
+int redLEDPin = 2;
 int redButtonPin = 9;
 int blueButtonPin = 8;
 int potiPin = 11;
+int pixelPin = 100;
 
 // Button Initialization
 Button redButton{redButtonPin};
@@ -19,36 +20,36 @@ void setup()
     pinMode(greenLEDPin, OUTPUT);
     pinMode(redLEDPin, OUTPUT);
     pinMode(potiPin, INPUT);
-    digitalWrite(greenLEDPin, HIGH);
-    digitalWrite(LED_BUILTIN, HIGH);
+    pinMode(LED_BUILTIN, OUTPUT);
 
     Serial.begin(115200);
 }
 
 void loop()
 {
+    Button::updateAll();
+    
     if (redButton.getState(redButton.click))
     {
-        Serial.println("Red button pressed");
-        if (digitalRead(LED_BUILTIN))
+        if (digitalRead(relayPin))
         {
-            digitalWrite(LED_BUILTIN, LOW);
+            digitalWrite(relayPin, LOW);
         } else
         {
-            digitalWrite(LED_BUILTIN, HIGH);
+            digitalWrite(relayPin, HIGH);
         }
     }
 
     if (blueButton.getState(blueButton.click))
     {
         Serial.println("Blue button pressed");
-        if (digitalRead(LED_BUILTIN))
+        if (digitalRead(greenLEDPin))
         {
-            digitalWrite(LED_BUILTIN, LOW);
+            digitalWrite(greenLEDPin, LOW);
         }
         else
         {
-            digitalWrite(LED_BUILTIN, HIGH);
+            digitalWrite(greenLEDPin, HIGH);
         }
     }
 }
